d503ad295ea4d0a03f16ff5f07040fd9fee8c153
Stepanwdr <stepanwdr@gmail.com>
2026-05-13 10:54:24 +0400

    port

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,12 +1,11 @@
 import authenticate from './authenticate.js';
 
 const PUBLIC_ROUTES = new Set([
-  'POST:api/auth/register',
-  'POST:api/auth/login',
-  'POST:api/auth/refresh',
-  'GET:api/',
+  'POST:/api/auth/register',
+  'POST:/api/auth/login',
+  'POST:/api/auth/refresh',
+  'GET:/api/',
 ]);
-
 export default function authorization(req, res, next) {
   const routeKey = `${req.method.toUpperCase()}:${req.path}`;
   console.log({routeKey})
